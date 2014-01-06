a068bd3bffd1a8387526f9ebe4a32411b823f601
kangliqiang <kangliq@163.com>
2014-01-06 13:21:04 +0800

    add example compile script

---
+++ example/simple/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+g++ -o ../../bin/producer Producer.cpp -g -m64 -I../../include -L../../bin -lrocketmq64 
+
+g++ -o ../../bin/pullconsumer PullConsumer.cpp -m64 -g -I../../include -L../../bin -lrocketmq64 
+
